512ecaa05b2253f4232132b10b96d53d1c2c53c8
Brainiaccc <52440972+jason51108@users.noreply.github.com>
2024-04-12 16:13:46 +0800

    Site updated: 2024-04-12 16:13:45

--- js/custom_mouse.js
+++ js/custom_mouse.js
@@ -1,36 +1,24 @@
-(function () {
-    var a_idx = 0;
-    window.onclick = function (event) {
-        var a = new Array("❤富强❤", "❤民主❤", "❤文明❤", "❤和谐❤", "❤自由❤", "❤平等❤", "❤公正❤", "❤法治❤", "❤爱国❤",
-            "❤敬业❤", "❤诚信❤", "❤友善❤");
-        var heart = document.createElement("b"); //创建b元素
-        heart.onselectstart = new Function('event.returnValue=false'); //防止拖动
-        document.body.appendChild(heart).innerHTML = a[a_idx]; //将b元素添加到页面上
-        a_idx = (a_idx + 1) % a.length;
-        heart.style.cssText = "position: fixed;left:-100%;"; //给p元素设置样式
-        var f = 16, // 字体大小
-            x = event.clientX - f / 2, // 横坐标
-            y = event.clientY - f, // 纵坐标
-            c = randomColor(), // 随机颜色
-            a = 1, // 透明度
-            s = 1.2; // 放大缩小
-        var timer = setInterval(function () { //添加定时器
-            if (a <= 0) {
-                document.body.removeChild(heart);
-                clearInterval(timer);
-            } else {
-                heart.style.cssText = "font-size:16px;cursor: default;position: fixed;color:" +
-                    c + ";left:" + x + "px;top:" + y + "px;opacity:" + a + ";transform:scale(" +
-                    s + ");";
-                y--;
-                a -= 0.016;
-                s += 0.002;
-            }
-        }, 15)
+if (!window.runningTime) {
+    window.runningTime = () => {
+        const infoBox = document.querySelector('.footer .website-info-box')
+        const tempDiv = document.createElement('div');
+        tempDiv.setAttribute('class', 'info-item default')
+        infoBox.appendChild(tempDiv)
+
+        const since = '2022-01-01 00:00:00'
+        const formatTimestamp = (timestamp) => {
+            const now = Date.now()
+            const timeDiff = Math.abs(now - timestamp)
+            const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
+            const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+            const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))
+            const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000)
+            return `${days} 天 ${hours} 小时 ${minutes} 分 ${seconds} 秒`
+        }
+
+        setInterval(() => {
+            tempDiv.innerHTML = '本站已安全运行 ' + formatTimestamp(new Date(since).getTime())
+        }, 1000)
     }
-    // 随机颜色
-    function randomColor() {
-        return "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math
-            .random() * 255)) + ")";
-    }
-}());
+}
+window.runningTime()
